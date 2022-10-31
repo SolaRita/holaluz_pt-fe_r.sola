5cc18f66ec44cdb5b39d97366f7bdf66ac6bed74
SolaRita <ritasolacanals@gmail.com>
2022-10-31 22:36:07 +0100

    redo search input to console log de value

--- src/components/inputSearch/index.js
+++ src/components/inputSearch/index.js
@@ -16,19 +16,24 @@ export default function InputSearch() {
         <fieldset>
             <h1>¿Quieres formar parte de la revolución de los tejados?</h1>
     
-            <form className="search" method="post" name="calculadora descuentos" >
+            <form onSubmit={ev => {
+                ev.preventDefault(); 
+                console.log(ev.target.cups.value)}}
+                className="search" method="post" name="calculadora descuentos" >
+
                 <label>Introduce aquí tu numero CUPS: </label>
                 <input
                     ref={inputCUPS}
+                    autocompleate = 'off'
                     type="text"
-                    name="nombre"
+                    name="cups"
                     label="Name"
                     id="CUPS"
                     placeholder="XXXX"
                     className="input"
                     />
                 
-                <button onClick={handleSearch} className="button">Iniciar revolución</button>
+                <button onClick={handleSearch} className="button" type='submit'>Iniciar revolución</button>
 
             </form>
         </fieldset>
